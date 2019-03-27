f9a617d265de9c1154c4249ba28e2de2bf87f908
Aasim Kandrikar <aasimk@umich.edu>
2019-03-27 12:22:28 -0400

    Migrated project to Swift 5

--- KeyboardActivityTracker/Views/KeyboardActivityView.swift
+++ KeyboardActivityTracker/Views/KeyboardActivityView.swift
@@ -196,6 +196,8 @@ extension NSBezierPath {
             case .lineTo: path.addLine(to: points[0])
             case .curveTo: path.addCurve(to: points[2], control1: points[0], control2: points[1])
             case .closePath: path.closeSubpath()
+            @unknown default:
+                fatalError("Unknown case for NSBezierPath")
             }
         }
         return path
