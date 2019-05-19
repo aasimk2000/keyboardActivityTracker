c692efa470cf9e03c2e87fd7d4e1da754b9f5e94
Aasim Kandrikar <aasimk@umich.edu>
2019-05-19 20:27:48 +0530

    Converted graph view from using core graphics to core animation.

--- KeyboardActivityTracker/Views/GraphView.swift
+++ KeyboardActivityTracker/Views/GraphView.swift
@@ -19,7 +19,7 @@ import Cocoa
         static let circleDiameter: CGFloat = 5.0
     }
     
-    @IBInspectable var startColor = NSColor(calibratedRed: 250/255, green: 193/255, blue: 153/255, alpha: 1) {
+    @IBInspectable var startColor: NSColor = NSColor(calibratedRed: 250/255, green: 193/255, blue: 153/255, alpha: 1) {
         didSet {
             needsDisplay = true
         }
@@ -29,7 +29,7 @@ import Cocoa
             needsDisplay = true
         }
     }
-    var graphPoints = [1, 1, 1, 1, 0, 0] {
+    var graphPoints: [Int] {
         didSet {
             needsDisplay = true
         }
@@ -41,105 +41,165 @@ import Cocoa
         }
     }
     
-    override func draw(_ dirtyRect: NSRect) {
-        let width = dirtyRect.width
-        let height = dirtyRect.height
-        
-        let path = NSBezierPath(roundedRect: dirtyRect,
-                                xRadius: Constants.cornerRadius,
-                                yRadius: Constants.cornerRadius)
-        path.addClip()
-        
-        let context = NSGraphicsContext.current?.cgContext
-        let colors = [startColor.cgColor, endColor.cgColor]
-        let colorSpace = CGColorSpaceCreateDeviceRGB()
-        
-        let colorLocations: [CGFloat] = [0.0, 1.0]
-        
-        let gradient = CGGradient(colorsSpace: colorSpace,
-                                  colors: colors as CFArray,
-                                  locations: colorLocations)!
-        
-        let startPoint = CGPoint.zero
-        let endPoint = CGPoint(x: 0, y: bounds.height)
-        context?.drawLinearGradient(gradient,
-                                    start: startPoint,
-                                    end: endPoint,
-                                    options: [])
-        
-        let margin = Constants.margin
-        let graphWidth = width - margin * 2 - 4
+    override var wantsUpdateLayer: Bool {
+        get {
+            return true
+        }
+    }
+    
+    let graphLayer = CAShapeLayer()
+    let gridLinesLayer = CAShapeLayer()
+    let circlesLayer = CALayer()
+    let gradientLayer = CAGradientLayer()
+    
+    var width: CGFloat { return frame.width }
+    var height: CGFloat { return frame.height }
+    
+    var margin: CGFloat { return Constants.margin }
+    var graphWidth: CGFloat { return width - margin * 2 - 4 }
+    var topBorder: CGFloat { return Constants.topBorder }
+    var bottomBorder: CGFloat { return Constants.bottomBorder }
+    var graphHeight: CGFloat { return height - topBorder - bottomBorder }
+    var maxValue: Int { return graphPoints.max() ?? 0 }
+    
+    
+    private func configureView() {
+        wantsLayer = true
+        layerContentsRedrawPolicy = .onSetNeedsDisplay
+    }
+    
+    init(frame frameRect: NSRect, points: [Int]) {
+        graphPoints = points
+        super.init(frame: frameRect)
+        configureView()
+        gradientLayer.frame = frameRect
+        setupLayers()
+    }
+    
+    func setupLayers() {
+        gradientLayer.colors = [startColor.cgColor, endColor.cgColor]
+        gradientLayer.cornerRadius = Constants.cornerRadius
+        gradientLayer.masksToBounds = true
+        layer?.addSublayer(gradientLayer)
         let columnXPoint = { (column: Int) -> CGFloat in
-            let spacing = graphWidth / CGFloat(self.graphPoints.count - 1)
-            return CGFloat(column) * spacing + margin + 2
+            let spacing = self.graphWidth / CGFloat(self.graphPoints.count - 1)
+            return CGFloat(column) * spacing + self.margin + 2
         }
         
-        let topBorder = Constants.topBorder
-        let bottomBorder = Constants.bottomBorder
-        let graphHeight = height - topBorder - bottomBorder
-        let maxValue = graphPoints.max() ?? 0 // Change this to not forcibly unwrap
         let columnYPoint = { (graphPoint: Int) -> CGFloat in
-            let y = CGFloat(graphPoint) / CGFloat(maxValue) * graphHeight
-            return graphHeight + topBorder - y
+            let y = CGFloat(graphPoint) / CGFloat(self.maxValue) * self.graphHeight
+            return self.graphHeight + self.topBorder - y
         }
         
-        NSColor.white.setFill()
-        NSColor.white.setStroke()
-        
-        let graphPath = NSBezierPath()
+        let graphPath = CGMutablePath()
         graphPath.move(to: CGPoint(x: columnXPoint(0),
-                                   y: columnYPoint(graphPoints.first ?? 0))) // Again change to not force unwrap
+                                   y: columnYPoint(graphPoints.first ?? 0)))
         
         for (i, j) in graphPoints[1...].enumerated() {
             let nextPoint = CGPoint(x: columnXPoint(i+1), y: columnYPoint(j))
-            graphPath.line(to: nextPoint)
+            graphPath.addLine(to: nextPoint)
         }
-        context?.saveGState()
         
-        let clippingPath = graphPath.copy() as! NSBezierPath
-        
-        clippingPath.line(to: CGPoint(x: columnXPoint(graphPoints.count - 1), y:height))
-        clippingPath.line(to: CGPoint(x: columnXPoint(0), y:height))
-        clippingPath.close()
-        
-        clippingPath.addClip()
-        
-        
-        let highestYPoint = columnYPoint(maxValue)
-        let graphStartPoint = CGPoint(x: margin, y: highestYPoint)
-        let graphEndPoint = CGPoint(x: margin, y: bounds.height)
-        
-        context?.drawLinearGradient(gradient, start: graphStartPoint, end: graphEndPoint, options: [])
-        context?.restoreGState()
-        graphPath.lineWidth = 2.0
-        graphPath.stroke()
+        graphLayer.path = graphPath
+        graphLayer.lineWidth = 2
+        graphLayer.strokeColor = NSColor.white.cgColor
+        graphLayer.fillColor = .clear
+        graphLayer.lineCap = .round
         
         for (i, j) in graphPoints.enumerated() {
             var point = CGPoint(x: columnXPoint(i), y: columnYPoint(j))
+            let circleLayer = CAShapeLayer()
             point.x -= Constants.circleDiameter / 2
             point.y -= Constants.circleDiameter / 2
             
-            let circle = NSBezierPath(ovalIn: CGRect(origin: point,
-                                                     size: CGSize(width: Constants.circleDiameter,
-                                                                  height: Constants.circleDiameter)))
-            circle.fill()
+            let circle = CGPath(ellipseIn: CGRect(origin: point,
+                                                  size: CGSize(width: Constants.circleDiameter, height: Constants.circleDiameter)), transform: nil)
+            circleLayer.path = circle
+            circleLayer.fillColor = NSColor.white.cgColor
+            circleLayer.strokeColor = NSColor.white.cgColor
+            circleLayer.lineWidth = 0.8
+            circlesLayer.addSublayer(circleLayer)
         }
         
-        let linePath = NSBezierPath()
-        
+        let linePath = CGMutablePath()
         linePath.move(to: CGPoint(x: margin, y: topBorder))
-        linePath.line(to: CGPoint(x: width - margin, y: topBorder))
+        linePath.addLine(to: CGPoint(x: width - margin, y: topBorder))
         
         linePath.move(to: CGPoint(x: margin, y: graphHeight / 2 + topBorder))
-        linePath.line(to: CGPoint(x: width - margin, y: graphHeight / 2 + topBorder))
+        linePath.addLine(to: CGPoint(x: width - margin, y: graphHeight / 2 + topBorder))
         
         linePath.move(to: CGPoint(x: margin, y: height - bottomBorder))
-        linePath.line(to: CGPoint(x: width - margin, y: height - bottomBorder))
+        linePath.addLine(to: CGPoint(x: width - margin, y: height - bottomBorder))
+        gridLinesLayer.path = linePath
+        gridLinesLayer.lineWidth = 1.0
         let color = NSColor(white: 1.0, alpha: Constants.colorAlpha)
-        color.setStroke()
+        gridLinesLayer.strokeColor = color.cgColor
         
-        linePath.lineWidth = 1.0
-        linePath.stroke()
+        gradientLayer.addSublayer(graphLayer)
+        gradientLayer.addSublayer(gridLinesLayer)
+        gradientLayer.addSublayer(circlesLayer)
+    }
+    
+    override func updateLayer() {
+        super.updateLayer()
+        gradientLayer.colors = [startColor.cgColor, endColor.cgColor]
+        let columnXPoint = { (column: Int) -> CGFloat in
+            let spacing = self.graphWidth / CGFloat(self.graphPoints.count - 1)
+            return CGFloat(column) * spacing + self.margin + 2
+        }
         
+        let columnYPoint = { (graphPoint: Int) -> CGFloat in
+            let y = CGFloat(graphPoint) / CGFloat(self.maxValue) * self.graphHeight
+            return self.graphHeight + self.topBorder - y
+        }
+
+        let graphPath = CGMutablePath()
+        graphPath.move(to: CGPoint(x: columnXPoint(0),
+                                   y: columnYPoint(graphPoints.first ?? 0)))
+        
+        for (i, j) in graphPoints[1...].enumerated() {
+            let nextPoint = CGPoint(x: columnXPoint(i+1), y: columnYPoint(j))
+            graphPath.addLine(to: nextPoint)
+        }
+        
+        graphLayer.path = graphPath
+        
+        //        let highestYPoint = columnYPoint(maxValue)
+        //        let graphStartPoint = CGPoint(x: margin, y: highestYPoint)
+        //        let graphEndPoint = CGPoint(x: margin, y: bounds.height)
+        
+        circlesLayer.sublayers = nil
+        
+        for (i, j) in graphPoints.enumerated() {
+            var point = CGPoint(x: columnXPoint(i), y: columnYPoint(j))
+            let circleLayer = CAShapeLayer()
+            point.x -= Constants.circleDiameter / 2
+            point.y -= Constants.circleDiameter / 2
+            
+            let circle = CGPath(ellipseIn: CGRect(origin: point,
+                                                  size: CGSize(width: Constants.circleDiameter, height: Constants.circleDiameter)), transform: nil)
+            circleLayer.path = circle
+            circleLayer.fillColor = NSColor.white.cgColor
+            circleLayer.strokeColor = NSColor.white.cgColor
+            circleLayer.lineWidth = 0.8
+            circlesLayer.addSublayer(circleLayer)
+        }
+    }
+    
+    override convenience init(frame frameRect: NSRect) {
+        self.init(frame: frameRect, points: [1, 2, 3, 4, 5, 6, 7])
+    }
+    
+    required init?(coder decoder: NSCoder) {
+        graphPoints = [1, 2, 3, 4, 5, 6, 7]
+        super.init(coder: decoder)
+        configureView()
+        gradientLayer.frame = bounds
+        setupLayers()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupLayers()
     }
 }
